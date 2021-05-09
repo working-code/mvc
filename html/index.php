dbb9399c6a46acccc88b7c695c5f55c1f0a0d2b0
Galushka Vladislav <galushka.vp@gmail.com>
2021-05-10 00:49:04 +0700

    off display error

--- html/index.php
+++ html/index.php
@@ -4,8 +4,8 @@ use App\Core\Application;
 
 require_once '../vendor/autoload.php';
 
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL | E_NOTICE);
+//ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL | E_NOTICE);
 
 $app = new Application();
 $app->run();
